77a4b4b4ee43de3bcb5ae243f99e065377c733c1
ShornRose <42686222+ShornRose@users.noreply.github.com>
2021-04-16 00:43:54 -0500

    Add files via upload

---
+++ Rosenburg.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang='en'>
+
+<head>
+	<meta charset="UTF-8">
+	<title>Rosenburg WebApp</title>
+</head>
+
+<body>
+<h1>Rosenburg Living Applications</h1>
+
+<p><span>Please select the following options.</span></p>
+<p><a href="bmiCalculator.php"><b>BMI Calculator</b></a></p>
+<p><a href="retirementCalculator.php"><b>Retirement Age Calculator </b></a></p>
+</body>
+
+</html>
---
+++ retirementCalculator.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang='en'>
+
+<head>
+	<meta charset="UTF-8">
+	<title>Rosenburg WebApp - Retirement</title>
+</head>
+
+<body>
+<a href="Rosenburg.php"><b>Home Page</b></a>
+</body>
+
+</html>
