3c57a7d6d8ba33677bed97e2d368b30b98bc44e5
ShornRose <42686222+ShornRose@users.noreply.github.com>
2021-04-16 22:21:48 -0500

    Add files via upload

--- bmiCalculator.php
+++ bmiCalculator.php
@@ -4,9 +4,128 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Rosenburg WebApp - BMI Calculator</title>
+	<style>
+        .error {
+          color: #FF0000;
+        }
+    </style>
 </head>
 
 <body>
+<h1><b>BMI Calculator</b></h1>
+<p><span>Please enter the following information.</span></p>
+
+	<?php
+		function sanitizeString($var)
+		{
+			$var = stripslashes($var);
+			$var = strip_tags($var);
+			$var = htmlentities($var);
+			$var = trim($var);
+			return $var;
+		}
+		
+		//initialize variables
+		$weight = "";
+		$weightErr = "";
+		
+		$feet = "";
+		$feetErr = "";
+		
+		$inches = "";
+		$inchesErr = "";
+		
+		$bmiAnswer = "????";
+		$categoryAnswer = "????";
+		
+		if (isset($_POST['Submit']))
+		{
+			$noErr = true;
+			
+			//validate weight value
+			$temp = sanitizeString($_POST['weight']);
+			if ((filter_var($temp, FILTER_VALIDATE_INT) === false) or ($temp < 0))
+			{
+				$noErr = false;
+				$weightErr = "Must be a non-negative integer";
+				$weight = "";
+			}
+			else
+			{
+				$weight = $temp;
+				$weightErr = "";
+			}
+			
+			//validate feet value
+			$temp = sanitizeString($_POST['feet']);
+			if ((filter_var($temp, FILTER_VALIDATE_INT) === false) or ($temp < 0))
+			{
+				$noErr = false;
+				$feetErr = "Must be a non-negative integer";
+				$feet = "";
+			}
+			else
+			{
+				$feet = $temp;
+				$feetErr = "";
+			}
+			
+			//validate inches value
+			$temp = sanitizeString($_POST['inches']);
+			if ((filter_var($temp, FILTER_VALIDATE_INT) === false) or ($temp < 0))
+			{
+				$noErr = false;
+				$inchesErr = "Must be a non-negative integer";
+				$inches = "";
+			}
+			else
+			{
+				$inches = $temp;
+				$inchesErr = "";
+			}
+			
+			//Run calculation if all inputs are valid.
+			if ($noErr == false)
+			{
+				$bmiAnswer = "????";
+				$categoryAnswer = "????";
+			}
+			else
+			{
+				$totalInches = $inches + ($feet * 12);
+				$bmiAnswer = round((703 * $weight) / pow($totalInches, 2), 1);
+				
+				if ($bmiAnswer < 18.5) $categoryAnswer = "Underweight";
+				else if ($bmiAnswer < 25) $categoryAnswer = "Normal Weight";
+				else if ($bmiAnswer < 30) $categoryAnswer = "Overweight";
+				else $categoryAnswer = "Obese";
+			}
+		}
+	?>
+
+	<form method="post" action="bmiCalculator.php">
+	
+		Weight(in pounds): <input type="text" size="3" name="weight" value="<?php echo $weight ?>">
+		<span class="error"><?php echo $weightErr ?></span>
+		<br><br>
+		
+		Height(feet): <input type="text" size="3" name="feet" value="<?php echo $feet ?>">
+		<span class="error"><?php echo $feetErr ?></span>
+		<br><br>
+		
+		Height(inches): <input type="text" size="3" name="inches" value="<?php echo $inches ?>">
+		<span class="error"><?php echo $inchesErr ?></span>
+		<br><br>
+		
+		<input type="submit" name="Submit" value="Calculate">
+		<br><br>
+		
+	</form>
+	
+<p>Your current BMI is <b><?php echo $bmiAnswer ?></b>, Category:<b><?php echo $categoryAnswer ?></b><p>
+<br>
+<br>
+
 <a href="Rosenburg.php"><b>Home Page</b></a>
 </body>
 
--- retirementCalculator.php
+++ retirementCalculator.php
@@ -4,9 +4,152 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Rosenburg WebApp - Retirement</title>
+	<style>
+        .error {
+          color: #FF0000;
+        }
+    </style>
 </head>
 
 <body>
+<h1><b>Retirement Age Calculator</b></h1>
+<p><span>Please enter the following information.</span></p>
+
+	<?php
+		function sanitizeString($var)
+		{
+			$var = stripslashes($var);
+			$var = strip_tags($var);
+			$var = htmlentities($var);
+			$var = trim($var);
+			return $var;
+		}
+		
+		//initialize variables to be nothing
+		$salary = "";
+		$salaryErr = "";
+		
+		$percent = "";
+		$percentErr = "";
+		
+		$currentAge = "";
+		$currentAgeErr = "";
+		
+		$goal = "";
+		$goalErr = "";
+		
+		$ageAnswer = "????";
+		
+		//run if user presses submit button
+		if (isset($_POST['Submit']))
+		{
+			$noErr = true;
+			
+			//validate salary value
+			$temp = sanitizeString($_POST['salary']);
+			if ((filter_var($temp, FILTER_VALIDATE_FLOAT) === false) or ($temp < 0))
+			{
+				$salary = "";
+				$salaryErr = "Enter a non-negative numeral";
+				$noErr = false;
+			}
+			else
+			{
+				$salary = $temp;
+				$salaryErr = "";
+			}
+			
+			//validate percent value
+			$temp = sanitizeString($_POST['percent']);
+			if ((filter_var($temp, FILTER_VALIDATE_FLOAT) === false) or ($temp < 0) or ($temp > 100))
+			{
+				$percent = "";
+				$percentErr = "Enter a number between 1 and 100";
+				$noErr = false;
+			}
+			else
+			{
+				$percent = $temp;
+				$percentErr = "";
+			}
+			
+			//validate currentAge value
+			$temp = sanitizeString($_POST['currentAge']);
+			if ((filter_var($temp, FILTER_VALIDATE_INT) === false) or ($temp < 1) or ($temp > 99))
+			{
+				$currentAge = "";
+				$currentAgeErr = "Enter a number between 1 and 99";
+				$noErr = false;
+			}
+			else
+			{
+				$currentAge = $temp;
+				$currentAgeErr = "";
+			}
+			
+			//validate goal value
+			$temp = sanitizeString($_POST['goal']);
+			if ((filter_var($temp, FILTER_VALIDATE_FLOAT) === false) or ($temp < 1))
+			{
+				$goal = "";
+				$goalErr = "Enter a non-negative numeral";
+				$noErr = false;
+			}
+			else
+			{
+				$goal = $temp;
+				$goalErr = "";
+			}
+			
+			//Run calculation if all inputs are valid
+			
+			if ($noErr == false)
+			{
+				$ageAnswer = "????";
+			}
+			else
+			{
+				$percentofSalary = $percent / 100;
+				$yearlySavings = $salary * $percentofSalary * 1.35;
+				$years = round($goal / $yearlySavings);
+				$goalAge = $years + $currentAge;
+				
+				if ($goalAge > 100)
+				{
+					$ageAnswer = "<span class='error'>Your goal is not possible in your lifetime.</span>";
+				}
+				else $ageAnswer = $goalAge;
+			}
+		}
+	?>
+
+	<form method="post" action="retirementCalculator.php">
+	
+		Yearly Salary: $<input type="text" size="20" name="salary" value="<?php echo $salary ?>">
+		<span class="error"><?php echo $salaryErr ?></span>
+		<br><br>
+		
+		Percent of Salary Saved: <input type="text" size="3" name="percent" value="<?php echo $percent ?>">%
+		<span class="error"><?php echo $percentErr ?></span>
+		<br><br>
+		
+		Your Current Age: <input type="text" size="3" name="currentAge" value="<?php echo $currentAge ?>">
+		<span class="error"><?php echo $currentAgeErr ?></span>
+		<br><br>
+		
+		How much do you plan to save for retirement?: $<input type="text" size="20" name="goal" value="<?php echo $goal ?>">
+		<span class="error"><?php echo $goalErr ?></span>
+		<br><br>
+		
+		<input type="submit" name="Submit" value="Calculate">
+		<br><br>
+		
+	</form>
+
+<p>At your current rate, you will be able to retire at the age of: <b><?php echo $ageAnswer ?></b></p>
+<br>
+<br>
+
 <a href="Rosenburg.php"><b>Home Page</b></a>
 </body>
 
